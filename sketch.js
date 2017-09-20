a6d645e563311583b57206798eebed050001c397
clarkmcadoo <clark.mcadoo@gmail.com>
2017-09-20 15:43:06 -0500

    made square that can move via arrow keys

--- sketch.js
+++ sketch.js
@@ -1,12 +1,28 @@
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(800, 600);
 }
 
+var value = 0;
+var x = 25;
+var y = 25;
+
 function draw() {
-  if (mouseIsPressed) {
-    fill(255,0,0);
-  } else {
-    fill(150, 150, 0);
+  background(255);
+  fill(value);
+  rect(x, y, 50, 50);
+}
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    x -= 50;
+    console.log(x,y);
+  } else if (keyCode === RIGHT_ARROW) {
+    x += 50;
+    console.log(x,y);
+  } else if (keyCode === UP_ARROW) {
+    y -= 50;
+    console.log(x,y);
+  } else if (keyCode === DOWN_ARROW) {
+    y += 50;
+    console.log(x,y);
   }
-  ellipse(mouseX, mouseY, 10, 10);
 }
