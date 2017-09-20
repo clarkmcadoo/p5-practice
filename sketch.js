14dbc48e50d6c8cba18d143c9fc63500a73879a1
clarkmcadoo <clark.mcadoo@gmail.com>
2017-09-20 14:36:06 -0500

    altered colors and size of ellipse

--- sketch.js
+++ sketch.js
@@ -4,9 +4,9 @@ function setup() {
 
 function draw() {
   if (mouseIsPressed) {
-    fill(0);
+    fill(255,0,0);
   } else {
-    fill(255);
+    fill(150, 150, 0);
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  ellipse(mouseX, mouseY, 10, 10);
 }
